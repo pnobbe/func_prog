92183ad1178d8e0d6811ee501d221a6c5ba0e6b7
Patrick Nobbe <patrick.nobbe@hotmail.com>
2017-09-18 19:57:00 +0200

    t

--- Setup.hs
+++ Setup.hs
@@ -22,8 +22,7 @@ main :: IO ()
 main = interact (lines >>> exercise >>> unlines)
 
 exercise :: [String] -> [String]
-exercise = parseTable >>> select "gender" "male"
-                      >>> project ["last", "first", "salary"] >>> printTable
+exercise = parseTable >>> select "gender" "male" >>> project ["last", "first", "salary"] >>> printTable
 
 -- | Parsing
 
@@ -38,44 +37,53 @@ parseTable s | null s    = []
 
 -- * Exercise 2
 
-printLine :: [Int] -> String
+printLine :: [Int] -> String -- Opinions?
 printLine xs | null xs   = []
              | otherwise = concat $ "+" : [ replicate x '-' ++ "+" | x <- xs ]
 
 -- * Exercise 3
 
 printField :: Int -> String -> String
-printField n s | all (\x -> isDigit x) s = spaces ++ s
+printField n s | all isDigit s = spaces ++ s
                | otherwise               = s ++ spaces
                where spaces = replicate (n - length s) ' '
 
 -- * Exercise 4
                
-printRow :: [(Int, String)] -> String
-printRow ((n, s):[]) = "|"
-printRow ((n, s):xs) = intercalate "|" [ printField n s | ]
+printRow :: [(Int, String)] -> String -- Feel like the implementation 'looks messy'
+printRow []             = []
+printRow xss@((_, _):_) = "|" ++ intercalate "|" (map (\(n, s) -> uncurry printField (n, s)) xss) ++ "|"
 
 -- * Exercise 5
 
 columnWidths :: Table -> [Int]
-columnWidths = undefined
+columnWidths t = [ maximum $ map length x | x <- transpose t ]
 
 -- * Exercise 6
 
-printTable :: Table -> [String]
-printTable table@(header:rows)
-    = undefined
+printTable :: Table -> [String] -- Could probably be done better, but how?
+printTable []                  = []
+printTable table@(header:rows) =
+    [ line, printRow $ zip maxWidths [map toUpper x | x <- header], line] ++ -- Formatted header
+    [ printRow n | n <- map (zip maxWidths) rows] ++ [line] -- Formatted rows
+    where maxWidths  = columnWidths table
+          line       = printLine maxWidths
 
 -- | Querying
 
 -- * Exercise 7
 
-select :: Field -> Field -> Table -> Table
+select :: Field -> Field -> Table -> Table -- Is this a good implementation of optional values?
 select column value table@(header:rows)
-    = undefined
+    | isNothing hIndex = table
+    | isJust hIndex    = header : [ x | x <- rows, x !! fromJust hIndex == value ]
+    where hIndex = elemIndex column header
+
 
 -- * Exercise 8
 
 project :: [Field] -> Table -> Table
 project columns table@(header:_)
-    = undefined
+    = transpose [ t !! i | i <- indices]
+    where t       = transpose table
+          indices = mapMaybe (`elemIndex` header) columns
